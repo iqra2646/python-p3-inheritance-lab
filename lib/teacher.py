e5f1bfc33e22031571e6f1c5e7a5b604226c66b4
iqra <iqralul9@gmail.com>
2024-08-29 14:55:55 +0300

    done assigment

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,24 @@
-#!/usr/bin/env python
+# lib/teacher.py
 
 from user import User
-
 import random
 
 class Teacher(User):
+    knowledge = [
+        "str is a data type in Python",
+        "programming is hard, but it's worth it",
+        "JavaScript async web request",
+        "Python function call definition",
+        "object-oriented teacher instance",
+        "programming computers hacking learning terminal",
+        "pipenv install pipenv shell",
+        "pytest -x flag to fail fast",
+    ]
+    
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = Teacher.knowledge
 
     def teach(self):
-        pass
+        # Return a random element from self.knowledge
+        return random.choice(self.knowledge)
